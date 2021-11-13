afcfc89dccaef5a0438c15e14965d81dc38b2eff
cesarchembi <cesar44@gmail.com>
2021-11-13 11:15:58 -0500

    Pruebas HU003 listar musicos

---
+++ app/src/androidTest/java/co/edu/uniandes/fourbidden/vinilos/vista/PruebaListarMusicos.kt
@@ -0,0 +1,79 @@
+package co.edu.uniandes.fourbidden.vinilos.vista
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import co.edu.uniandes.fourbidden.vinilos.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class PruebaListarMusicos {
+
+    @get:Rule
+    val mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun pruebaListarMusicos() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.bingreso), withText("Entrar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+        Thread.sleep(5000)
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.btMusico), withText("Músicos"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+        Thread.sleep(5000)
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
---
+++ app/src/androidTest/java/co/edu/uniandes/fourbidden/vinilos/vista/PruebaRegresoListaAlbumes.kt
@@ -0,0 +1,96 @@
+package co.edu.uniandes.fourbidden.vinilos.vista
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import co.edu.uniandes.fourbidden.vinilos.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class PruebaRegresoListaAlbumes {
+
+    @get:Rule
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun pruebaRegresoListaAlbumes() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.bingreso), withText("Entrar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+        Thread.sleep(5000)
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.btMusico), withText("Músicos"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+        Thread.sleep(5000)
+
+        val materialButton3 = onView(
+            allOf(
+                withId(R.id.btAlbumes), withText("Álbumes"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton3.perform(click())
+        Thread.sleep(5000)
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
