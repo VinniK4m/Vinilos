29e8d11f0cb7866ccba202e10c725a554a3640c7
vinni <vinni9@gmail.com>
2021-12-04 11:12:53 -0500

    commit CF08: Se inicia contruccion de adicion de track a album

---
+++ app/src/main/java/co/edu/uniandes/fourbidden/vinilos/vista/CrearTrackActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.uniandes.fourbidden.vinilos.vista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.edu.uniandes.fourbidden.vinilos.R
+
+class CrearTrackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_track)
+    }
+}
